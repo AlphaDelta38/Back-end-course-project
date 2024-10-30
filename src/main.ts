aead906f6932ca04d17ea7150c01d5e56c4afa08
AlphaDelta38 <kiril.morozov.01@gmail.com>
2024-10-30 20:37:30 +0300

    inicializate proejct and give settings him

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import * as process from "node:process";
+
+
+
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('API Documentation of HospitalDB')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('#documentation', app, document);
+
+  await app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`server started on port ${process.env.PORT}`)
+  });
+
+}
+bootstrap();
