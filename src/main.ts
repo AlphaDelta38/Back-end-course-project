faefbf46e76ff652cb1f1889d7382651f129e20a
AlphaDelta38 <kiril.morozov.01@gmail.com>
2024-10-30 20:46:49 +0300

    change path for documentation

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
       .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('#documentation', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT || 3000, ()=>{
     console.log(`server started on port ${process.env.PORT}`)
